45420998d0fcd200d6c22543065640515f725f39
ADubeys <arun.dubey@infocentric.com.au>
2023-05-31 18:24:21 +1000

    First

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# FloodML
-FloodML is a web application that uses Machine Learning to predict floods based on weather and and various historical data.
+# Car-Price Prediction
--- app.py
+++ app.py
@@ -1,97 +1,52 @@
-"""Web app."""
-import flask
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask,render_template,request
+import numpy as np
 import pickle
-import base64
-from training import prediction
-import requests
-app = flask.Flask(__name__)
 
-data = [{'name':'Delhi', "sel": "selected"}, {'name':'Mumbai', "sel": ""}, {'name':'Kolkata', "sel": ""}, {'name':'Bangalore', "sel": ""}, {'name':'Chennai', "sel": ""}]
-# data = [{'name':'India', "sel": ""}]
-months = [{"name":"May", "sel": ""}, {"name":"June", "sel": ""}, {"name":"July", "sel": "selected"}]
-cities = [{'name':'Delhi', "sel": "selected"}, {'name':'Mumbai', "sel": ""}, {'name':'Kolkata', "sel": ""}, {'name':'Bangalore', "sel": ""}, {'name':'Chennai', "sel": ""}, {'name':'New York', "sel": ""}, {'name':'Los Angeles', "sel": ""}, {'name':'London', "sel": ""}, {'name':'Paris', "sel": ""}, {'name':'Sydney', "sel": ""}, {'name':'Beijing', "sel": ""}]
 
-model = pickle.load(open("model.pickle", 'rb'))
+app = Flask(__name__)
+model = pickle.load(open('Forest_model.pkl','rb'))
+
 
 @app.route("/")
-@app.route('/index.html')
-def index() -> str:
-    """Base page."""
-    return flask.render_template("index.html")
+def hello():
+    return render_template("index.html")
 
-@app.route('/plots.html')
-def plots():
-    return render_template('plots.html')
 
-@app.route('/heatmaps.html')
-def heatmaps():
-    return render_template('heatmaps.html')
-
-@app.route('/satellite.html')
-def satellite():
-    direc = "processed_satellite_images/Delhi_July.png"
-    with open(direc, "rb") as image_file:
-        image = base64.b64encode(image_file.read())
-    image = image.decode('utf-8')
-    return render_template('satellite.html', data=data, image_file=image, months=months, text="Delhi in January 2020")
-
-@app.route('/satellite.html', methods=['GET', 'POST'])
-def satelliteimages():
-    place = request.form.get('place')
-    date = request.form.get('date')
-    data = [{'name':'Delhi', "sel": ""}, {'name':'Mumbai', "sel": ""}, {'name':'Kolkata', "sel": ""}, {'name':'Bangalore', "sel": ""}, {'name':'Chennai', "sel": ""}]
-    months = [{"name":"May", "sel": ""}, {"name":"June", "sel": ""}, {"name":"July", "sel": ""}]
-    for item in data:
-        if item["name"] == place:
-            item["sel"] = "selected"
+@app.route("/predict",methods=['POST'])
+def predict():
+    inp_fea = list(request.form.values())
+    # inp_fea = np.asarray(inp_fea)
+    # inp_fea = ",".join(inp_fea)
+    # inp_fea = list(inp_fea)
+    if inp_fea[3]=='Petrol':
+        inp_fea[3] = 0
+    elif inp_fea[3] == 'Diesel':
+        inp_fea[3] = 1
+    else:
+        inp_fea[3] = 2
     
-    for item in months:
-        if item["name"] == date:
-            item["sel"] = "selected"
+    if inp_fea[4]=='Dealer':
+        inp_fea[4] = 0
+    else:
+        inp_fea[4]= 1
+    
+    if inp_fea[5]=='Manual':
+        inp_fea[5] = 0
+    else:
+        inp_fea[5]= 1
 
-    text = place + " in " + date + " 2020"
 
-    direc = "processed_satellite_images/{}_{}.png".format(place, date)
-    with open(direc, "rb") as image_file:
-        image = base64.b64encode(image_file.read())
-    image = image.decode('utf-8')
-    return render_template('satellite.html', data=data, image_file=image, months=months, text=text)
+    inp_fea = np.asarray(inp_fea)
+    inp_fea = inp_fea.reshape(1,-1)
+    inp_fea = list(inp_fea)
 
-@app.route('/predicts.html')
-def predicts():
-    return render_template('predicts.html', cities=cities, cityname="Information about the city")
+    # final_fea = [np.array(inp_fea)]
+    prediction = model.predict(inp_fea)
+    output = round(prediction[0],2)
 
-@app.route('/predicts.html', methods=["GET", "POST"])
-def get_predicts():
-    try:
-        cityname = request.form["city"]
-        cities = [{'name':'Delhi', "sel": ""}, {'name':'Mumbai', "sel": ""}, {'name':'Kolkata', "sel": ""}, {'name':'Bangalore', "sel": ""}, {'name':'Chennai', "sel": ""}, {'name':'New York', "sel": ""}, {'name':'Los Angeles', "sel": ""}, {'name':'London', "sel": ""}, {'name':'Paris', "sel": ""}, {'name':'Sydney', "sel": ""}, {'name':'Beijing', "sel": ""}]
-        for item in cities:
-            if item['name'] == cityname:
-                item['sel'] = 'selected'
-        print(cityname)
-        URL = "https://geocode.search.hereapi.com/v1/geocode"
-        location = cityname
-        api_key = 'LHtLOXHyYWrAYEFNbXIQg8BM6PEY8Jryp88mmHYvAGw' # Acquire from developer.here.com
-        PARAMS = {'apikey':api_key,'q':location} 
-        # sending get request and saving the response as response object 
-        r = requests.get(url = URL, params = PARAMS) 
-        data = r.json()
-        latitude = data['items'][0]['position']['lat']
-        longitude = data['items'][0]['position']['lng']
-        final = prediction.get_data(latitude, longitude)
+    return render_template('index.html',prediction_text = "The price of this car should be :".format(output))
+    # return render_template('index.html',prediction_text = "{}".format(inp_fea))
 
-        final[4] *= 15
-        if str(model.predict([final])[0]) == "0":
-            pred = "Safe"
-        else:
-            pred = "Unsafe"
-        
-        return render_template('predicts.html', cityname="Information about " + cityname, cities=cities, temp=round(final[0], 2), maxt=round(final[1], 2), wspd=round(final[2], 2), cloudcover=round(final[3], 2), percip=round(final[4], 2), humidity=round(final[5], 2), pred = pred)
-    except:
-        return render_template('predicts.html', cities=cities, cityname="Oops, we weren't able to retrieve data for that city.")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
